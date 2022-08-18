284028e973bb9c4a6a8be28154be05d56b11a023
Marilzon <maxmaril@hotmail.com>
2022-08-17 21:32:41 -0300

    prefixxes and sufixes binds add to componemts

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 	<div>
 		<header class="header text-center">
 			<h1>V NAME GENERATOR <span class="fa fa-check text-success"></span></h1>
-			<h5>Gerador de nomes para seu projeto</h5>
+			<h5>Gerador de nomes para seu dominio</h5>
 			<div class="tags-stacks">
 				<span class="badge text-bg-success m-1">Vue.js</span>
 				<span class="badge text-bg-success m-1">GraphQL</span>
@@ -13,51 +13,22 @@
 		<div id="main">
 			<div class="container">
 				<div class="row">
-
 					<div class="col">
-						<h5>Prefixos <span class="badge bg-secondary">{{ prefixes.length }}</span></h5>
-						<div class="card-body">
-							<ul class="list-group">
-								<li class="list-group-item d-flex justify-content-between align-items-center"
-									v-for="prefix in prefixes" v-bind:key="prefix">
-									{{ prefix }}
-									<button class="btn text-muted" v-on:click="deletePrefix(prefix)"><i
-											class="fa fa-trash"></i></button>
-								</li>
-							</ul>
-						</div>
-						<div class="input-group my-1">
-							<input type="text" v-model="prefix" v-on:keyup.enter="addPrefix(prefix)"
-								class="form-control" placeholder="Prefixo">
-							<div class="input-group-prepend">
-								<button class="btn btn-outline-success mx-1" v-on:click="addPrefix(prefix)">
-									<i class="fa fa-plus"></i>
-								</button>
-							</div>
-						</div>
+                        <ItemList 
+							title="Prefixos"
+							v-bind:items="prefixes" 
+							v-on:addItem="addPrefix"
+							v-on:deleteItem="deletePrefix"
+					></ItemList>
 					</div>
 
 					<div class="col">
-						<h5>Sufixos <span class="badge bg-secondary">{{ sufixes.length }}</span></h5>
-						<div class="card-body">
-							<ul class="list-group">
-								<li class="list-group-item d-flex justify-content-between align-items-center"
-									v-for="sufix in sufixes" v-bind:key="sufix">
-									{{ sufix }}
-									<button class="btn text-muted" v-on:click="deleteSufix(sufix)"><i
-											class="fa fa-trash"></i></button>
-								</li>
-							</ul>
-						</div>
-						<div class="input-group my-1">
-							<input type="text" v-model="sufix" v-on:keyup.enter="addSufix(sufix)" class="form-control"
-								placeholder="Sufixo">
-							<div class="input-group-prepend">
-								<button class="btn btn-outline-success mx-1" v-on:click="addSufix(sufix)">
-									<i class="fa fa-plus"></i>
-								</button>
-							</div>
-						</div>
+                        <ItemList 
+							title="Sufixos"
+							v-bind:items="sufixes"
+							v-on:addItem="addSufix"
+							v-on:deleteItem="deleteSufix"
+						></ItemList>
 					</div>
 				</div>
 
@@ -82,7 +53,6 @@
 						</ul>
 					</div>
 				</div>
-
 			</div>
 		</div>
 	</div>
@@ -91,6 +61,7 @@
 <script>
 import "bootstrap/dist/css/bootstrap.css"
 import "font-awesome/css/font-awesome.css"
+import ItemList from "./components/ItemList.vue"
 
 export default {
 	name: "App",
@@ -100,41 +71,44 @@ export default {
 			sufix: "",
 			prefixes: [],
 			sufixes: []
-		}
+		};
 	},
 	methods: {
 		addPrefix(prefix) {
-			this.prefixes.push(prefix)
-			this.prefix = ""
+			this.prefixes.push(prefix);
+			this.prefix = "";
 		},
 		addSufix(sufix) {
-			this.sufixes.push(sufix)
-			this.sufix = ""
+			this.sufixes.push(sufix);
+			this.sufix = "";
 		},
 		deletePrefix(prefix) {
-			this.prefixes.splice(this.prefixes.indexOf(prefix), 1)
+			this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
 		},
 		deleteSufix(sufix) {
-			this.sufixes.splice(this.sufixes.indexOf(sufix), 1)
+			this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
 		},
 	},
 	computed: {
 		domains() {
-			const domains = []
+			const domains = [];
 			for (let prefix of this.prefixes) {
 				for (let sufix of this.sufixes) {
-					const url = (prefix + sufix).toLowerCase()
+					const url = (prefix + sufix).toLowerCase();
 					const checkout = `
 						https://cart.hostgator.com.br/?pid=d&sld=${url}&tld=.com.br
-					`
+					`;
 					domains.push({
 						url,
 						checkout
-					})
+					});
 				}
 			}
-			return domains
+			return domains;
 		}
+	},
+	components: {
+		ItemList
 	}
 }
 </script>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<h5>{{ title }} <span class="badge bg-secondary">{{ items.length }}</span></h5>
+		<div class="card-body">
+			<ul class="list-group">
+				<li class="list-group-item d-flex justify-content-between align-items-center" v-for="item in items"
+					v-bind:key="item">
+					{{ item }}
+					<button class="btn text-muted" v-on:click="deleteItem(item)">
+						<i class="fa fa-trash"></i>
+					</button>
+				</li>
+			</ul>
+		</div>
+		<div class="input-group my-1">
+			<input type="text" v-model="item" v-on:keyup.enter="addItem(item)" class="form-control"
+				placeholder="Prefixo">
+			<div class="input-group-prepend">
+				<button class="btn btn-outline-success mx-1" v-on:click="addItem(item)">
+					<i class="fa fa-plus"></i>
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ItemList",
+	props: ["title", "items"],
+	data() {
+		return {
+			item: ""
+		}
+	},
+	methods: {
+		addItem(item) {
+			this.$emit("addItem", item)
+			this.item = ""
+		},
+		deleteItem(item) {
+			this.$emit("deleteItem", item)
+		},
+
+	}
+}
+</script>
+
+<style scoped>
+</style>
